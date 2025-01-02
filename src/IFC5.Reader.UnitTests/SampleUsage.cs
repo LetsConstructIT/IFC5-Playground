c574caa9c89168c4ed1639acef7b6c57a2a305b1
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2025-01-02 19:31:31 +0100

    Created UnitTests project to present usage in one place

---
+++ src/IFC5.Reader.UnitTests/SampleUsage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IFC5.Reader.UnitTests
+{
+    [TestClass]
+    public class SampleUsage
+    {
+        [TestMethod]
+        public void ReadingFromFile()
+        {
+            var path = @"C:\Users\grzeg\Documents\IFC5\hello-wall.ifcx";
+
+            var composedObjects = new Reader().Read(path);
+        }
+    }
+}
