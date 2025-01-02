36ed5e440c7a6f74daa65f8ae523df922436aba1
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2025-01-02 19:27:07 +0100

    Removed default comments from Rhino Plugin

--- src/IFC5.RhinoImport/RhinoImportCommand.cs
+++ src/IFC5.RhinoImport/RhinoImportCommand.cs
@@ -1,30 +1,20 @@
 ﻿using Rhino;
 using Rhino.Commands;
-using Rhino.Input;
-using Rhino.Input.Custom;
-using System;
-using System.Collections.Generic;
 
 namespace IFC5.RhinoImport;
 public class RhinoImportCommand : Command
 {
     public RhinoImportCommand()
     {
-        // Rhino only creates one instance of each command class defined in a
-        // plug-in, so it is safe to store a refence in a static property.
         Instance = this;
     }
 
-    ///<summary>The only instance of this command.</summary>
     public static RhinoImportCommand Instance { get; private set; }
 
-    ///<returns>The command name as it appears on the Rhino command line.</returns>
     public override string EnglishName => "IFC5ImportCommand";
 
     protected override Result RunCommand(RhinoDoc doc, RunMode mode)
     {
-        // Usually commands in import plug-ins are used to modify settings and behavior.
-        // The import work itself is performed by the Ifc5ImportPlugin class.
         return Result.Success;
     }
 }
