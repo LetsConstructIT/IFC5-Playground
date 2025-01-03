54ff6764ebf17eb83c94b253bceaab5f803f539b
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2025-01-03 11:16:00 +0100

    Sketch for materials

--- src/IFC5.RhinoImport/Ifc5Inserter.cs
+++ src/IFC5.RhinoImport/Ifc5Inserter.cs
@@ -1,9 +1,12 @@
 ﻿using Rhino.Geometry;
 using Rhino;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using IFC5.Reader.Composers;
 using IFC5.Reader.Models;
+using System;
+using Eto.Forms;
 
 namespace IFC5.RhinoImport;
 internal class Ifc5Inserter
@@ -13,27 +16,42 @@ internal class Ifc5Inserter
     internal void Insert(RhinoDoc doc, ComposedObjects composedObjects)
     {
         var transformation = Transform.Identity;
+        var material = Color.White;
+
         foreach (var composedObject in composedObjects)
         {
-            AddMesh(doc, composedObject, transformation);
+            AddMesh(doc, composedObject, transformation, material);
         }
     }
 
-    private void AddMesh(RhinoDoc doc, ComposedObject composedObject, Transform transformation)
+    private void AddMesh(RhinoDoc doc, ComposedObject composedObject, Transform transformation, Color material)
     {
         transformation = AdjustTransformation(composedObject.Components, transformation);
+        material = AdjustMaterial(composedObject.Components, material);
 
         if (HasVisibleMesh(composedObject.Components, out UsdGeomMeshComponent? usdGeomMesh) && usdGeomMesh is not null)
         {
             var mesh = _meshCreator.CreateRhinoMesh(usdGeomMesh);
             mesh.Transform(transformation);
+            mesh.VertexColors.CreateMonotoneMesh(System.Drawing.Color.FromArgb(128, 0, 128, 0));
             doc.Objects.Add(mesh);
         }
 
         foreach (var child in composedObject.Children)
         {
-            AddMesh(doc, child, transformation);
+            AddMesh(doc, child, transformation, material);
         }
+    }
+
+    private Color AdjustMaterial(List<ComponentJson> components, Color material)
+    {
+        var materialBindings = components.OfType<UsdShadeMaterialBindingApiComponent>();
+        if (!materialBindings.Any())
+            return material;
+
+        var materialBinding = materialBindings.Last();
+
+        return material;
     }
 
     private Transform AdjustTransformation(List<ComponentJson> components, Transform transformation)
