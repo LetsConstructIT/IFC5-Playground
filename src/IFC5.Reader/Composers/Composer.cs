630799a9aeb08344d7a7504429b26d1b5e1d9aa0
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2025-01-03 12:59:51 +0100

    Comments to composer

--- src/IFC5.Reader/Composers/Composer.cs
+++ src/IFC5.Reader/Composers/Composer.cs
@@ -6,10 +6,13 @@ internal class Composer
 {
     public ComposedObjects Compose(IEnumerable<PrimJson> prims)
     {
+        // let's bring out children and inherits
         var flattenedTree = new TreeFlattener(prims).Flatten();
 
+        // graph composition with help of DFS
         var rootPrims = new TreeComposer(flattenedTree).Compose();
 
+        // collapsing classes and attaching overs to defs
         var composedObjects = new DefsComposer(rootPrims, flattenedTree.GetOvers()).Compose();
 
         return composedObjects;
